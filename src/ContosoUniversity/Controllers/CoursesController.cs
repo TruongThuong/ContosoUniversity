136cee0de035eaf39675d73e80929a68f0cc716e
THUONG\IO_OI <thuongtruong2702@gmail.com>
2016-10-04 11:15:49 +0700

    Edit Course Create of Course Controller

--- src/ContosoUniversity/Controllers/CoursesController.cs
+++ src/ContosoUniversity/Controllers/CoursesController.cs
@@ -58,13 +58,23 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CourseID,Credits,Title")] Course course)
+        public async Task<IActionResult> Create([Bind("Credits,Title")] Course course)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    _context.Add(course);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
             }
             return View(course);
         }
